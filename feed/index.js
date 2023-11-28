5cb14861cccd655d57bf3950ee66a78402ece84b
ErikHHJ <erikhhju@gmail.com>
2023-11-28 05:45:41 +0100

    swapped out bootstrap and popper.js script tags with a bundle.js tag. Also changed colour of logout button

--- feed/index.js
+++ feed/index.js
@@ -5,6 +5,7 @@ const loginOrLogout = document.querySelector(".loginOrLogout");
 if (localStorage.getItem("token")) {
   loginOrLogout.href = "#";
   loginOrLogout.innerHTML = "Logout";
+  loginOrLogout.style.color = "red";
   loginOrLogout.addEventListener("click", () => {
     localStorage.removeItem("token");
     localStorage.removeItem("name");
